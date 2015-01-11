7031b0ed18acb58a454adbff84dd4204474da4a5
Jon Bellah <jbellah@gmail.com>
2015-01-11 17:15:18 -0600

    Style adjustments

--- functions.php
+++ functions.php
@@ -102,9 +102,8 @@ add_action( 'widgets_init', 'origin_widgets_init' );
 function origin_scripts() {
 	wp_enqueue_style( 'origin-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'origin-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'origin-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'origin-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'origin-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -116,10 +115,10 @@ add_action( 'wp_enqueue_scripts', 'origin_scripts' );
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.php';
+require get_template_directory() . '/includes/template-tags.php';
 
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
+require get_template_directory() . '/includes/customizer.php';
 

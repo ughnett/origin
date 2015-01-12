1287b8f8cbec5f45283f6fb644cad1bbbab99d8a
Jon Bellah <jbellah@gmail.com>
2015-01-12 10:33:08 -0600

    Updating versioning in stylesheet and markup spacing in header

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
+<head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
